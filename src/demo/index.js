908044f9f686934020b73598f83cd97a25b5ee46
Rong Sen Ng <wes.ngrongsen@gmail.com>
2018-03-20 00:09:22 +0800

    🔖 0.4.2

---
+++ src/demo/index.js
@@ -0,0 +1,26 @@
+// @ts-check
+
+/** Import other modules */
+const { ntml } = require('../..');
+
+/** Setting up */
+const html = ntml({
+  parseHtml: true,
+  minify: true,
+});
+const tidyHtml = ntml({
+  parseHtml: true,
+  minify: false,
+});
+
+(async function main() {
+  try {
+    const greetings = async () => 'Hello, World!';
+    const minified = await html`<div>${greetings}</div>`;
+    const rendered = await tidyHtml`<div>${greetings}</div>`;
+
+    console.log('# %s\n\n%s', minified, rendered);
+  } catch (e) {
+    console.error('^ Error', e);
+  }
+})()
