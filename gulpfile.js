6be142f26e33ee3b6bdcb2ed6444f456ef5b2a50
Rong Sen Ng <wes.ngrongsen@gmail.com>
2018-04-01 22:45:27 +0800

    ✅ To fix broken test by including missing gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const builder = require('@messageflow/build').builder({
+  dist: '.',
+  cleanGlobs: ['./*.js', './*.d.ts', '!./gulpfile.js', '!**/json.d.ts'],
+});
+
+gulp.task('lint', builder.lint);
+gulp.task('default', gulp.series(...[builder.clean, builder.lint, builder.ts]));
