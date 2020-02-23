9932aeac681bdea3d6dbb15693160da124452ef4
Rong Sen Ng <wes.ngrongsen@gmail.com>
2020-02-23 17:32:02 +0800

    Revert "feat: Remove rollup for build and use ESM build"
    This reverts commit bf4c8f5c18669e2083c4471b30a50725fc1145e1.

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import {
   serialize,
 } from 'parse5';
 
-import { parsePartial } from './parse-partial.js';
-
 // export const DEFAULT_MINIFY_OPTIONS: htmlMinifier.Options = {
 //   collapseBooleanAttributes: true,
 //   collapseWhitespace: true,
@@ -23,11 +21,39 @@ import { parsePartial } from './parse-partial.js';
 //   trimCustomFragments: true,
 // };
 
-const parser = parsePartial(serialize);
+async function processLiterals(strings: TemplateStringsArray, ...exps: any[]): Promise<string> {
+  const listTask = exps.map(async (n) => {
+    const tasks = (Array.isArray(n) ? n : [n])
+      .map(async o => 'function' === typeof(o) ? o() : o);
+
+    return Promise.all(tasks);
+  });
+  const done = await Promise.all(listTask);
+  const doneLen = done.length;
+
+  return strings.reduce((p, n, i) => {
+    const nTask = done[i] ;
+    const joined = Array.isArray(nTask) ? nTask.join('') : nTask;
+    return `${p}${i >= doneLen ? n : `${n}${joined}`}`;
+  }, '');
+}
+
+async function parsePartial(
+  fn: typeof parse | typeof parseFragment,
+  strings: TemplateStringsArray,
+  ...exps: any[]
+) {
+  try {
+    const content = await processLiterals(strings, ...exps);
+    return serialize(fn(content));
+  } catch (e) {
+    throw e;
+  }
+}
 
 export const html = async (s: TemplateStringsArray, ...e: any[]) =>
-  parser(c => parse(`<!doctype html>${c}`), s, ...e);
+  parsePartial(c => parse(`<!doctype html>${c}`), s, ...e);
 export const htmlFragment = async (s: TemplateStringsArray, ...e: any[]) =>
-  parser(parseFragment, s, ...e);
+  parsePartial(parseFragment, s, ...e);
 
 export default { html, htmlFragment };
