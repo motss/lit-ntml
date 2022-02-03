c471e17dd1c63f24172e3a7593eb9b54a4cca92f
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-02-03 15:21:37 +0800

    build: update build config

--- postinstall.bash
+++
@@ -1,4 +0,0 @@
-if [[ \"$CI\" != 'true' ]] && [[ \"$INIT_CWD\" != *'node_modules/lit-ntml' ]] && [[ \"$INIT_CWD\" == *'lit-ntml' ]]
-then
-  simple-git-hooks && npm dedupe
-fi
---
+++ postinstall.mjs
@@ -0,0 +1,23 @@
+#!/usr/bin/env zx
+
+const {
+  CI,
+  INIT_CWD,
+} = process.env;
+
+const {
+  name: moduleName,
+} = JSON.parse(await fs.readFile('./package.json', { encoding: 'utf-8' }));
+
+console.debug({
+  CI,
+  INIT_CWD,
+});
+
+if (
+  CI != 'true' &&
+  !INIT_CWD.endsWith(`node_modules/${moduleName}`) &&
+  INIT_CWD.endsWith(moduleName)
+) {
+  await $`simple-git-hooks && npm dedupe`;
+}
