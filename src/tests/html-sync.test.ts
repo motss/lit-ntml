3a8d0993d9cd363ad6ee3ced3af31962df77c2be
The web walker <wes.ngrongsen@gmail.com>
2022-06-26 18:19:31 +0800

    refactor: Migrate to Vitest (#60)
    * test: migrate to vitest
    * build: update test.coverage.reporter
    * 3.0.1-rc.0
    * chore: update dependencies
    * ci: update CI script
    
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- src/tests/html-sync.test.ts
+++ src/tests/html-sync.test.ts
@@ -1,5 +1,4 @@
-import { test } from 'uvu';
-import * as assert from 'uvu/assert';
+import { expect, it } from 'vitest';
 
 import { htmlSync as html } from '..';
 
@@ -11,49 +10,46 @@ const peopleList = [
   'Cash Black',
 ];
 
-test(`throws when error happens`, () => {
+it(`throws when error happens`, () => {
   try {
-    // tslint:disable-next-line: no-unused-expression
     html`${() => { throw new Error('error'); }}`;
   } catch (e) {
-    assert.equal(e, new Error('error'));
+    expect(e).toEqual(new Error('error'));
   }
 });
 
-test(`renders`, () => {
+it(`renders`, () => {
   const d = html`${helloWorld}`;
 
-  assert.is(d, `<!DOCTYPE html><html><head></head><body><h1>Hello, World!</h1></body></html>`);
+  expect(d).toBe(`<!DOCTYPE html><html><head></head><body><h1>Hello, World!</h1></body></html>`);
 });
 
-test(`renders with sync tasks`, () => {
+it(`renders with sync tasks`, () => {
   const syncTask = () => helloWorld;
   const syncLiteral = 'John Doe';
   const d = html`<section>${syncTask}<h2>${syncLiteral}</h2></section>`;
 
-  assert.is(d, `<!DOCTYPE html><html><head></head><body><section><h1>Hello, World!</h1><h2>John Doe</h2></section></body></html>`);
+  expect(d).toBe(`<!DOCTYPE html><html><head></head><body><section><h1>Hello, World!</h1><h2>John Doe</h2></section></body></html>`);
 });
 
-test(`renders with async tasks`, () => {
+it(`renders with async tasks`, () => {
   const asyncTask = async () => helloWorld;
   const asyncLiteral = Promise.resolve('John Doe');
   const d = html`<section>${asyncTask}<h2>${asyncLiteral}</h2></section>`;
 
-  assert.is(d, `<!DOCTYPE html><html><head></head><body><section>[object Promise]<h2>[object Promise]</h2></section></body></html>`);
+  expect(d).toBe(`<!DOCTYPE html><html><head></head><body><section>[object Promise]<h2>[object Promise]</h2></section></body></html>`);
 });
 
-test(`renders with a mixture of sync + async tasks`, async () => {
+it(`renders with a mixture of sync + async tasks`, async () => {
   const asyncTask = async () => helloWorld;
   const syncLiteral = await 'John Doe';
   const d = html`<section>${asyncTask}<h2>${syncLiteral}</h2></section>`;
 
-  assert.is(d, `<!DOCTYPE html><html><head></head><body><section>[object Promise]<h2>John Doe</h2></section></body></html>`);
+  expect(d).toBe(`<!DOCTYPE html><html><head></head><body><section>[object Promise]<h2>John Doe</h2></section></body></html>`);
 });
 
-test(`renders a list of sync tasks`, () => {
+it(`renders a list of sync tasks`, () => {
   const d = html`${helloWorld}<ul>${peopleList.map(n => `<li>${n}</li>`)}</ul>`;
 
-  assert.is(d, `<!DOCTYPE html><html><head></head><body><h1>Hello, World!</h1><ul><li>John Doe</li><li>Michael CEO</li><li>Vict Fisherman</li><li>Cash Black</li></ul></body></html>`);
+  expect(d).toBe(`<!DOCTYPE html><html><head></head><body><h1>Hello, World!</h1><ul><li>John Doe</li><li>Michael CEO</li><li>Vict Fisherman</li><li>Cash Black</li></ul></body></html>`);
 });
-
-test.run();
