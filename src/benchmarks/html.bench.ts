f4d2590dc2361a7aada80c882e45d3cbc51f5951
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2023-02-21 01:35:39 +0800

    chore: standardize test params in tests and benchmarks
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- src/benchmarks/html.bench.ts
+++ src/benchmarks/html.bench.ts
@@ -1,48 +1,19 @@
-import { bench, describe } from 'vitest';
+import { bench } from 'vitest';
 
-import { html } from '..';
+import { html } from '../index.js';
+import { testParamsForHtml } from '../tests/constants.js';
 
-const helloWorld = `<h1>Hello, World!</h1>`;
-const peopleList = [
-  'John Doe',
-  'Michael CEO',
-  'Vict Fisherman',
-  'Cash Black',
-];
-
-describe(html.name, () => {
-  bench('renders', async () => {
-    await html`<h1>Hello, World!</h1>`;
+testParamsForHtml
+  .map(({ inputFn, message }) => ({ inputFn, message }))
+  .forEach(({
+    inputFn,
+    message,
+  }) => {
+    bench(message, async () => {
+      try {
+        await inputFn(html);
+      } catch {
+        // no-op
+      }
+    });
   });
-
-  bench('renders with sync tasks', async () => {
-    const syncTask = () => helloWorld;
-    const syncLiteral = 'John Doe';
-
-    await html`<section>${syncTask}<h2>${syncLiteral}</h2></section>`;
-  });
-
-  bench('renders with async tasks', async () => {
-    const syncTask = async () => helloWorld;
-    const syncLiteral = Promise.resolve('John Doe');
-
-    await html`<section>${syncTask}<h2>${syncLiteral}</h2></section>`;
-  });
-
-  bench('renders with sync + async tasks', async () => {
-    const syncTask = async () => helloWorld;
-    const syncLiteral = Promise.resolve('John Doe');
-
-    await html`<section>${syncTask}<h2>${syncLiteral}</h2></section>`;
-  });
-
-  bench('renders with a list of sync tasks', async () => {
-    await html`${helloWorld}<ul>${peopleList.map(n => `<li>${n}</li>`)}</ul>`;
-  });
-
-  bench('renders external style', async () => {
-    const asyncExternalStyleTask = () => html`body { margin: 0; padding: 0; box-sizing: border-box; }`;
-
-    await html`<style>${asyncExternalStyleTask}</style>${helloWorld}`;
-  });
-});
--- src/tests/html-fragment.test.ts
+++ src/tests/html-fragment.test.ts
@@ -1,63 +1,30 @@
 import { expect, it } from 'vitest';
 
-import { htmlFragment as html } from '..';
+import { htmlFragment } from '../index.js';
+import { testParamsForHtmlFragment } from './constants.js';
 
-const helloWorld = `<h1>Hello, World!</h1>`;
-const peopleList = [
-  'John Doe',
-  'Michael CEO',
-  'Vict Fisherman',
-  'Cash Black',
-];
+testParamsForHtmlFragment.forEach(({
+  error,
+  expected,
+  inputFn,
+  message,
+}) => {
+  it(message, async () => {
+    try {
+      const result = await inputFn(htmlFragment);
 
-it(`throws when error happens`, async () => {
-  try {
-    const errorContent = async () => { throw new Error('error'); };
-    await html`${errorContent}`;
-  } catch (e) {
-    expect(e).toEqual(new Error('error'));
-  }
-});
-
-it(`renders`, async () => {
-  const d = await html`${helloWorld}`;
-
-  expect(d).toBe(`<h1>Hello, World!</h1>`);
-});
-
-it(`renders with sync tasks`, async () => {
-  const syncTask = () => helloWorld;
-  const syncLiteral = 'John Doe';
-  const d = await html`<section>${syncTask}<h2>${syncLiteral}</h2></section>`;
-
-  expect(d).toBe(`<section><h1>Hello, World!</h1><h2>John Doe</h2></section>`);
-});
-
-it(`renders with async tasks`, async () => {
-  const asyncTask = async () => helloWorld;
-  const asyncLiteral = Promise.resolve('John Doe');
-  const d = await html`<section>${asyncTask}<h2>${asyncLiteral}</h2></section>`;
-
-  expect(d).toBe(`<section><h1>Hello, World!</h1><h2>John Doe</h2></section>`);
-});
-
-it(`renders with a mixture of sync + async tasks`, async () => {
-  const asyncTask = async () => helloWorld;
-  const syncLiteral = Promise.resolve('John Doe');
-  const d = await html`<section>${asyncTask}<h2>${syncLiteral}</h2></section>`;
-
-  expect(d).toBe(`<section><h1>Hello, World!</h1><h2>John Doe</h2></section>`);
-});
-
-it(`renders a list of sync tasks`, async () => {
-  const d = await html`${helloWorld}<ul>${peopleList.map(n => `<li>${n}</li>`)}</ul>`;
-
-  expect(d).toBe(`<h1>Hello, World!</h1><ul><li>John Doe</li><li>Michael CEO</li><li>Vict Fisherman</li><li>Cash Black</li></ul>`);
-});
-
-it(`renders external style`, async () => {
-  const asyncExternalStyleTask = async () => html/* css */`body { margin: 0; padding: 0; box-sizing: border-box; }`;
-  const d = await html`<style>${asyncExternalStyleTask}</style>${helloWorld}`;
-
-  expect(d).toBe(`<style>body { margin: 0; padding: 0; box-sizing: border-box; }</style><h1>Hello, World!</h1>`);
+      if (error) {
+        expect.fail('it should throw an error');
+      } else {
+        expect(result).toBe(expected);
+      }
+    } catch (e) {
+      if (error) {
+        expect(e).toEqual(error);
+      } else if (e && error == null) {
+        console.error(e);
+        expect.fail('it should not throw any error');
+      }
+    }
+  });
 });
